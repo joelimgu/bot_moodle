848e00b4a2e2e37d8599cd9f186db56de6ceb1ce
joel <joelimgu@gmail.com>
2021-03-08 14:49:37 +0100

    some info added

--- xlsx_test.py
+++ xlsx_test.py
@@ -1,6 +1,6 @@
 import openpyxl
 # for more documentation continue reading at http://python-simple.com/python-autres-modules-non-standards/openpyxl.php
-
+# the directory for more info is at https://1drv.ms/u/s!Ak5XyBWR45TUsaRjH3MLEMBvH03ofQ?e=x311PM
 
 # document = xlrd.open_workbook("ressources_divers/Modele-FAQ-Vizir.xlsx")
 workbook = openpyxl.load_workbook('ressources_divers/Modele-FAQ-Vizir.xlsx')
